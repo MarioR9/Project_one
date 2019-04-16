77a19e0ce5199d80fd3a16bf724cd2c471833dda
Marior9 <mariorodriguezan@gmail.com>
2019-04-16 18:28:50 -0400

    create new member finished

--- lib/member.rb
+++ lib/member.rb
@@ -23,11 +23,28 @@ class Member < ActiveRecord::Base
 
     end 
 
-    def 
-    	#create an new member 
-    	
 
-    end 
+	def receive_new_user
+		
+		puts "Welcome"
+		puts "Hello!\nEnter your first name:"
+		answer_fn = STDIN.gets.chomp
+		puts "Enter your last name:"
+		answer_ln = STDIN.gets.chomp
+		puts "How old are you in years?"
+		answer_age = STDIN.gets.chomp.to_i
+		puts "Enter 1 for female \nEnter 2 for male \nEnter 3 for non-binary"
+		answer_g = STDIN.gets.chomp
+			if answer_g == 1
+				gender = "Female"
+			elsif answer_g == 2
+				gender = "Male"
+			else answer_g == 3
+				gender = "Non-binary"
+			end
+   new_member =  Member.create(first_name: answer_fn, last_name: answer_ln, age: answer_age, gender: gender)
+
+    end
 #     def most_popular_trainer
 # #finds the most popular trainer by going through all the instances of
 # #trainer and returns the trainer with the most sessions
